1d5f7ca9431902aefcfe1b8499abdfd5e121e338
Ian Ona <ianbenedictona@gmail.com>
2024-02-21 15:42:55 +0800

    Initial Commit

---
+++ ScreenSort/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenSort
+//
+//  Created by ianona on 2024/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenSort/ScreenSortApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenSortApp.swift
+//  ScreenSort
+//
+//  Created by ianona on 2024/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ScreenSortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
